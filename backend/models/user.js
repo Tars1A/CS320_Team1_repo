f299f306eb4656de420cf387eaacdc96fbb86e9f
tinholeong020 <tinholeong@umass.edu>
2022-03-07 14:54:08 -0500

    update schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,6 @@
-import mongoose from 'mongoose';
+//import mongoose from 'mongoose';
+
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
@@ -18,4 +20,5 @@ const userSchema = new Schema({
 });
 
 const User = mongoose.model('User', userSchema);
-export default User 
+//export default User ;
+module.exports = User;
